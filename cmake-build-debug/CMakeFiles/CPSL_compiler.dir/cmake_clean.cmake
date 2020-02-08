30aeb033ebfc9100d01b0cb063b0cf7ca0d9cebc
Zion Steiner <zion.steiner@aggiemail.usu.edu>
2020-02-08 14:10:49 -0800

    Init commit

---
+++ cmake-build-debug/CMakeFiles/CPSL_compiler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPSL_compiler.dir/main.cpp.o"
+  "CPSL_compiler"
+  "CPSL_compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPSL_compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
